677b7b4a2c6cf68977b6f0b313520e58e40ee424
Mazoda <oabumutlak@gmail.com>
2023-02-22 21:03:35 +0200

    Vue events modified

--- Vue Events/app.js
+++ Vue Events/app.js
@@ -2,14 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      number: 1,
     };
   },
   methods: {
     add() {
-      this.counter++;
+      this.counter += this.number;
     },
     sub() {
-      this.counter--;
+      this.counter -= this.number;
     },
   },
 });
